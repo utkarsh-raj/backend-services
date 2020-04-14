77cc9ab7c723d34101dc1ad448c16adec7271aaf
Utkarsh Raj Singh <u.rajsingh2503@gmail.com>
2020-04-14 12:23:22 +0530

    Add cache cheking methods

--- app.js
+++ app.js
@@ -33,6 +33,29 @@ var jobSchema = new mongoose.Schema({
 
 // ===========================================================
 
+//Middleware Function to Check Cache
+checkCache = (req, res, next) => {
+    const { id } = req.params;
+
+    redis_client.get(id, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+        //if no match found
+        if (data != null) {
+            res.send(data);
+        } else {
+            //proceed to next middleware function
+            next();
+        }
+    });
+};
+
+// ===========================================================
+
+
+
 var port = process.env.PORT || 8090;
 
 app.listen(8090, process.env.IP, function (req, res) {
