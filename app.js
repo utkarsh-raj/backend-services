4657e8f94cf3ed749396d157037cf3e3f0ee5ea7
Utkarsh Raj Singh <u.rajsingh2503@gmail.com>
2020-04-14 15:17:14 +0530

    add the get and post routes

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ require('dotenv').config();
 var app = express();
 var redis_client = redis.createClient(process.env.PORT || 6379);
 
+redis_client.on('connect', function () {
+    console.log('connected');
+});
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 redis_client.auth(process.env.REDISPASSWORD);
@@ -26,34 +30,90 @@ mongoose.connect("mongodb+srv://" + process.env.DBUSERNAME + ":" + process.env.D
 
 // Models
 
-var jobSchema = new mongoose.Schema({
-    name: { type: String, default: 'New Job' }
+var showSchema = new mongoose.Schema({
+    name: { type: String, default: 'New Show' }
 });
+
+var Show = new mongoose.model("Show", showSchema);
 
 
 // ===========================================================
 
-//Middleware Function to Check Cache
-checkCache = (req, res, next) => {
-    const { id } = req.params;
+// ===========================================================
 
-    redis_client.get(id, (err, data) => {
+app.post("/shows/post", function (req, res) {
+    Show.create({
+        name: req.body.name
+    }, function (err, show) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json(error);
+        }
+        else {
+            var data = {
+                name: show.name,
+                id: show._id
+            }
+            var response = {
+                status: 'success',
+                message: ' Saved into the database',
+                data: data
+            }
+            redis_client.set(show._id.toString(), JSON.stringify(data), 'EX', 10, function(err, reply) {
+                console.log(reply);
+            });
+            return res.status(200).json(response);
+        }
+    });
+});
+
+app.get("/shows/get/:id", function(req, res) {
+    redis_client.get(req.params.id, (err, data) => {
         if (err) {
             console.log(err);
             res.status(500).send(err);
         }
         //if no match found
         if (data != null) {
-            res.send(data);
+            data = JSON.parse(data);
+            var response = {
+                status: 'success',
+                message: 'Show retrieved from the cache',
+                data: {
+                    name: data.name,
+                    id: data.id                
+                }
+            }
+            redis_client.set(data.id, JSON.stringify(data), 'EX', 10, function (err, reply) {
+                console.log(reply);
+            });
+
+            return res.status(200).json(response);
         } else {
-            //proceed to next middleware function
-            next();
+            Show.findById(req.params.id, function (err, show) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json(err);
+                }
+                else {
+                    var data = {
+                        name: show.name,
+                        id: show._id
+                    } 
+                    var response = {
+                        status: 'success',
+                        message: 'Show retrieved from the database',
+                        data: data
+                    }
+                    redis_client.set(show._id.toString(), JSON.stringify(data), 'EX', 10, function (err, reply) {
+                        console.log(reply);
+                    });
+                    return res.status(200).json(response);
+                }
+            });
         }
     });
-};
-
-// ===========================================================
-
+});
 
 
 var port = process.env.PORT || 8090;
